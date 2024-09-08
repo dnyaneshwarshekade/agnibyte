9f7440ff3f45cd6bba5faff447106a9856290a9b
Dnyaneshwar <agnibyte@gmail.com>
2024-09-09 01:16:45 +0530

    team type update

---
+++ src/types/team.ts
@@ -0,0 +1,10 @@
+// types/team.ts
+export interface TeamType {
+  id: number;
+  name: string;
+  designation: string;
+  image: string;
+  facebookLink: string;
+  twitterLink: string;
+  instagramLink: string;
+}
