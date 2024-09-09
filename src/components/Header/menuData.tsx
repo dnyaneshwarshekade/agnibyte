6c594de1c74305cf1c490fcd86589997ccb5b5e8
Dnyaneshwar <agnibyte@gmail.com>
2024-09-09 22:42:46 +0530

    removed unessory files

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -48,24 +48,7 @@ const menuData: Menu[] = [
         path: "/contact",
         newTab: false,
       },
-      {
-        id: 43,
-        title: "Blog Grid Page",
-        path: "/blog",
-        newTab: false,
-      },
-      {
-        id: 44,
-        title: "Blog Sidebar Page",
-        path: "/blog-sidebar",
-        newTab: false,
-      },
-      {
-        id: 45,
-        title: "Blog Details Page",
-        path: "/blog-details",
-        newTab: false,
-      },
+      
       {
         id: 46,
         title: "Sign In Page",
