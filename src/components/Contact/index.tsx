4edc35b2bb5e0c1d3764e58711976abd2b431135
Dnyaneshwar <agnibyte@gmail.com>
2024-09-09 11:56:29 +0530

    made a changes in contacus form

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -5,11 +5,13 @@ import { useState } from 'react';
 import emailjs from 'emailjs-com';
 import NewsLatterBox from './NewsLatterBox';
 
-// Your component logic here
 const Contact = () => {
   const [formData, setFormData] = useState({
-    name: "",
+    firstName: "",
+    lastName: "",
     email: "",
+    number: "",
+    service: "",
     message: "",
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -34,7 +36,14 @@ const Contact = () => {
       .then(
         (result) => {
           setSubmissionStatus("Success! Your message has been sent.");
-          setFormData({ name: "", email: "", message: "" });
+          setFormData({
+            firstName: "",
+            lastName: "",
+            email: "",
+            number: "",
+            service: "",
+            message: "",
+          });
         },
         (error) => {
           setSubmissionStatus("Error! Something went wrong.");
@@ -55,38 +64,47 @@ const Contact = () => {
               data-wow-delay=".15s"
             >
               <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
-                Need Help? Open a Ticket
+                Need help with IT solutions for your business? Fill Out Form!
               </h2>
               <p className="mb-12 text-base font-medium text-body-color">
-                Our support team will get back to you ASAP via email.
+                Share your requirements or problem, and we will get back to you via email as soon as possible.
               </p>
               <form onSubmit={handleSubmit}>
                 <div className="-mx-4 flex flex-wrap">
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
-                      <label
-                        htmlFor="name"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                      >
-                        Your Name
+                      <label htmlFor="firstName" className="mb-3 block text-sm font-medium text-dark dark:text-white">
+                        First Name*
                       </label>
                       <input
                         type="text"
-                        name="name"
-                        value={formData.name}
+                        name="firstName"
+                        value={formData.firstName}
                         onChange={handleChange}
-                        placeholder="Enter your name"
+                        placeholder="Enter your first name"
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       />
                     </div>
                   </div>
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
-                      <label
-                        htmlFor="email"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                      >
-                        Your Email
+                      <label htmlFor="lastName" className="mb-3 block text-sm font-medium text-dark dark:text-white">
+                        Last Name
+                      </label>
+                      <input
+                        type="text"
+                        name="lastName"
+                        value={formData.lastName}
+                        onChange={handleChange}
+                        placeholder="Enter your last name"
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label htmlFor="email" className="mb-3 block text-sm font-medium text-dark dark:text-white">
+                        Email*
                       </label>
                       <input
                         type="email"
@@ -98,20 +116,56 @@ const Contact = () => {
                       />
                     </div>
                   </div>
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label htmlFor="number" className="mb-3 block text-sm font-medium text-dark dark:text-white">
+                        Number
+                      </label>
+                      <input
+                        type="text"
+                        name="number"
+                        value={formData.number}
+                        onChange={handleChange}
+                        placeholder="Enter your phone number"
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
                   <div className="w-full px-4">
                     <div className="mb-8">
-                      <label
-                        htmlFor="message"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      <label htmlFor="service" className="mb-3 block text-sm font-medium text-dark dark:text-white">
+                        What are you looking for...
+                      </label>
+                      <select
+                        name="service"
+                        value={formData.service}
+                        onChange={handleChange}
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       >
-                        Your Message
+                        <option value="">Select a service</option>
+                        <option value="Web Hosting">Web Hosting</option>
+                        <option value="Application Development Android/iOS">Application Development Android/iOS</option>
+                        <option value="Website Development in Python, MERN, MEAN, PHP, Other">
+                          Website Development in Python, MERN, MEAN, PHP, Other
+                        </option>
+                        <option value="Email Services SMTP">Email Services SMTP</option>
+                        <option value="SMS Services SMS Campaign">SMS Services SMS Campaign</option>
+                        <option value="Share Marketing Advertise">Share Marketing Advertise</option>
+                        <option value="Other">Other</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <div className="mb-8">
+                      <label htmlFor="message" className="mb-3 block text-sm font-medium text-dark dark:text-white">
+                        Message*
                       </label>
                       <textarea
                         name="message"
                         rows={5}
                         value={formData.message}
                         onChange={handleChange}
-                        placeholder="Enter your Message"
+                        placeholder="Your message here..."
                         className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       ></textarea>
                     </div>
@@ -122,7 +176,7 @@ const Contact = () => {
                       className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark"
                       disabled={isSubmitting}
                     >
-                      {isSubmitting ? "Submitting..." : "Submit Ticket"}
+                      {isSubmitting ? "Submitting..." : "Submit"}
                     </button>
                   </div>
                 </div>
